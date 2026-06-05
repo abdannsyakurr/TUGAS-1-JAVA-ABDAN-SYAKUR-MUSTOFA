1abf59b0ed68fb52e06420ba38b53fd1993e8416
abdannsyakurr <abdansyakurr333@gmail.com>
2026-06-05 17:18:07 +0700

    Update GajiKaryawan.java

--- GajiKaryawan.java
+++ GajiKaryawan.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class HitungGajiKaryawan {
+public class GajiKaryawan {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
